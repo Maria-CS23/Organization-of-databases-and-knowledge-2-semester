ff9d325072b7b4a0ea83b9000ffc41ce3d0a6abb
Maria-CS23 <mariia.pikulina@student.karazin.ua>
2025-04-05 16:06:03 +0300

    pract4

--- practical work 4/QUERY.sql
+++ practical work 4/QUERY.sql
@@ -17,3 +17,16 @@ SELECT SUM(OrderAmount) AS TotalRevenue FROM Orders;
 SELECT AVG(Price) AS AveragePhonePrice FROM Phone;
 
 SELECT MAX(Price) AS MaxPrice, MIN(Price) AS MinPrice FROM Phone;
+
+-- 3 запити з використанням віконних функцій
+SELECT ClientID, OrderAmount,
+    SUM(OrderAmount) OVER (PARTITION BY ClientID) AS TotalPerClient
+FROM Orders;
+
+SELECT ClientID, OrderID, OrderDate,
+    ROW_NUMBER() OVER (PARTITION BY ClientID ORDER BY OrderDate) AS OrderNum
+FROM Orders;
+
+SELECT Manufacturer, Model, Specifications, Price,
+    LAG(Price) OVER (PARTITION BY Manufacturer ORDER BY Model) AS PreviousModelPrice
+FROM Phone;
