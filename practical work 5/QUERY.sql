16c2935f7eb4d5477aeb0e2e81d711b8b5ef752e
Maria-CS23 <mariia.pikulina@student.karazin.ua>
2025-04-22 20:13:16 +0300

    pract5

--- practical work 5/QUERY.sql
+++ practical work 5/QUERY.sql
@@ -50,3 +50,59 @@ ON Orders(ClientID)
 INCLUDE(OrderID, OrderAmount, OrderDate);
 
 SELECT OrderID, OrderAmount, OrderDate FROM Orders WHERE ClientID = 157;
+
+-- Завдання 9
+CREATE NONCLUSTERED INDEX IX_Phone_Filtered_Availability
+ON Phone(Availability)
+WHERE Availability = 'В наявності';
+
+-- Завдання 10
+SELECT 
+    OBJECT_NAME(i.object_id) AS TableName,
+    i.name AS IndexName,
+    ips.index_type_desc,
+    ips.avg_fragmentation_in_percent
+FROM sys.dm_db_index_physical_stats (DB_ID(), NULL, NULL, NULL, 'LIMITED') AS ips
+JOIN sys.indexes AS i ON ips.object_id = i.object_id AND ips.index_id = i.index_id
+WHERE i.name IS NOT NULL
+ORDER BY avg_fragmentation_in_percent DESC;
+
+-- Завдання 11
+ALTER INDEX IX_Client_RegistrationDate ON Client REORGANIZE;
+
+-- Завдання 12
+ALTER INDEX IX_Client_RegistrationDate ON Client REBUILD;
+
+-- Завдання 13
+CREATE NONCLUSTERED INDEX IX_Client_Type ON Client(Type);
+
+DROP INDEX IX_Client_Type ON Client;
+
+-- Завдання 14
+SELECT * FROM LegalEntity WHERE TaxID = '2934567891';
+
+CREATE UNIQUE INDEX IX_LegalEntity_TaxID ON LegalEntity(TaxID);
+
+SELECT * FROM LegalEntity WHERE TaxID = '2934567891';
+
+-- Завдання 15
+SELECT * FROM Orders WHERE OrderAmount > 5000 ORDER BY OrderDate;
+
+-- Завдання 16
+SELECT
+ DB_NAME() AS [База_даних],
+ OBJECT_NAME(i.[object_id]) AS [Таблиця],
+ i.name AS [Індекс],
+ i.type_desc AS [Тип_індексу],
+ i.is_unique AS [Унікальний],
+ ps.avg_fragmentation_in_percent AS [Фрагментація_у_%],
+ ps.page_count AS [Кількість_сторінок]
+FROM
+ sys.indexes AS i
+INNER JOIN
+ sys.dm_db_index_physical_stats(DB_ID(), NULL, NULL, NULL, 'LIMITED') AS ps
+ ON i.[object_id] = ps.[object_id] AND i.index_id = ps.index_id
+WHERE
+ i.type > 0 AND i.is_hypothetical = 0
+ORDER BY
+ ps.avg_fragmentation_in_percent DESC;
