e0e75021ea53ff9e54494a55335113cfea288a2e
Maria-CS23 <mariia.pikulina@student.karazin.ua>
2025-04-27 23:51:22 +0300

    lab 5

--- laboratory work 5/PROCEDURES.sql
+++ laboratory work 5/PROCEDURES.sql
@@ -6,3 +6,41 @@ EXEC sp_helpindex 'Orders';
 
 
 EXEC sp_columns 'Client';
+
+-- Завдання 7
+
+CREATE PROCEDURE ##GetAvailablePhones
+AS
+BEGIN
+    SELECT PhoneID, Manufacturer, Model, Price
+    FROM Phone
+    WHERE Availability = N'В наявності';
+END;
+GO
+
+
+EXEC ##GetAvailablePhones;
+
+
+CREATE PROCEDURE ##GetOrdersReport
+AS
+BEGIN
+    SELECT OrderID, ClientID, OrderDate, OrderAmount
+    FROM Orders
+    WHERE OrderDate > DATEADD(DAY, -30, GETDATE());
+END;
+GO
+
+EXEC ##GetOrdersReport;
+
+
+CREATE PROCEDURE ##GetDelivered
+AS
+BEGIN
+    SELECT DeliveryID, DeliveryStatus, DeliveryMethod
+    FROM Delivery
+    WHERE DeliveryStatus = N'Доставлено';
+END;
+GO
+
+EXEC ##GetDelivered;
