73cd5074a0b720e5489fb97c120d5f1b5d35dcbd
Maria-CS23 <mariia.pikulina@student.karazin.ua>
2025-04-28 00:25:32 +0300

    lab 5

--- laboratory work 5/PROCEDURES.sql
+++ laboratory work 5/PROCEDURES.sql
@@ -44,3 +44,64 @@ END;
 GO
 
 EXEC ##GetDelivered;
+
+-- Завдання 8
+
+CREATE PROCEDURE #FindPhonesByManufacturer
+    @Manufacturer NVARCHAR(50)
+AS
+BEGIN
+    SELECT PhoneID, Manufacturer, Model, Price
+    FROM Phone
+    WHERE Manufacturer = @Manufacturer;
+END;
+
+EXEC #FindPhonesByManufacturer @Manufacturer = 'Apple';
+
+
+CREATE PROCEDURE #AddIndividualClient
+    @FullName NVARCHAR(100),
+    @Address NVARCHAR(200),
+    @PhoneNumber VARCHAR(20),
+    @Email NVARCHAR(100) = NULL
+AS
+BEGIN
+    DECLARE @NewClientID INT;
+
+    SELECT @NewClientID = ISNULL(MAX(ClientID), 0) + 1 FROM Client;
+
+    INSERT INTO Client (ClientID, Type, RegistrationDate)
+    VALUES (@NewClientID, N'Фізична особа', GETDATE());
+
+    INSERT INTO Individual (ClientID, FullName, Address, PhoneNumber, Email)
+    VALUES (@NewClientID, @FullName, @Address, @PhoneNumber, @Email);
+
+    PRINT 'Додано клієнта ClientID = ' + CAST(@NewClientID AS NVARCHAR);
+END;
+GO
+
+EXEC #AddIndividualClient 
+    @FullName = 'Гриценко Ірина Миколаївна',
+    @Address = 'Харків, вул. Карла Маркса, 5',
+    @PhoneNumber = '380689785432',
+    @Email = 'grytsenko399@example.com';
+
+
+
+CREATE PROCEDURE #UpdateOrderStatus
+    @OrderID INT,
+    @NewStatus NVARCHAR(50)
+AS
+BEGIN
+    UPDATE Orders
+    SET OrderStatus = @NewStatus
+    WHERE OrderID = @OrderID;
+
+    PRINT 'Статус замовлення оновлено';
+END;
+GO
+
+EXEC #UpdateOrderStatus 
+    @OrderID = 132,
+    @NewStatus = 'В дорозі';
+
