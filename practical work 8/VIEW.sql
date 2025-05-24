929b21a6e73bf9f90440d082fa64268ef9b21a64
Maria-CS23 <mariia.pikulina@student.karazin.ua>
2025-05-24 18:43:31 +0300

    pract 8

--- practical work 8/VIEW.sql
+++ practical work 8/VIEW.sql
@@ -16,6 +16,7 @@ FROM LegalEntity l;
 -- Завдання 4
 SELECT ClientID, Name FROM vClientNames;
 
+
 -- Завдання 5
 CREATE VIEW vPremiumPhones AS
 SELECT *
@@ -80,15 +81,84 @@ SELECT * FROM vClientOrderCounts;
 
 -- Завдання 9
 
+CREATE VIEW vVIPClients AS
+SELECT ClientID, ClientName, OrderCount
+FROM vClientOrderCounts
+WHERE OrderCount > 3;
+
+
+SELECT * FROM vVIPClients;
+
+
 -- Завдання 10
+
+ALTER VIEW vClientOrders AS
+SELECT 
+    c.ClientID, 
+    c.Type AS ClientType,
+    CASE 
+        WHEN i.FullName IS NOT NULL THEN i.FullName
+        WHEN le.CompanyName IS NOT NULL THEN le.CompanyName
+        ELSE 'Невідомий клієнт'
+    END AS ClientName,
+    o.OrderID, 
+    o.OrderDate,
+    o.OrderAmount,
+    o.OrderStatus
+FROM Client c
+LEFT JOIN Individual i ON c.ClientID = i.ClientID
+LEFT JOIN LegalEntity le ON c.ClientID = le.ClientID
+JOIN Orders o ON c.ClientID = o.ClientID;
+
+
+SELECT * FROM vClientOrders;
+
 
 -- Завдання 11
 
+DROP VIEW vVIPClients;
+
 -- Завдання 12
+
+CREATE VIEW vOrderSummary AS
+SELECT 
+    OrderID, 
+    OrderAmount AS TotalCost, 
+    OrderDate AS PurchaseDate,
+    OrderStatus AS Status
+FROM Orders;
+
+
+SELECT * FROM vOrderSummary;
+
 
 -- Завдання 13
 
+CREATE VIEW vPhonePriceWithTax AS
+SELECT 
+    PhoneID,
+    Manufacturer,
+    Model,
+    Price AS BasePrice,
+    Price * 1.15 AS PriceWithTax
+FROM Phone;
+
+
+SELECT * FROM vPhonePriceWithTax;
+
+
 -- Завдання 14
+
+CREATE VIEW vExpensivePhones AS
+SELECT *
+FROM Phone
+WHERE Price > 800
+WITH CHECK OPTION;
+
+
+INSERT INTO vExpensivePhones (PhoneID, Manufacturer, Model, Specifications, Price, Availability) 
+VALUES (11, 'Xiaomi', 'Redmi 10', '64GB, Carbon Gray', 600, N'В наявності');
+
 
 -- Завдання 15
 
