8933714e2fc74fab5f6c92d4d61f096f09b5b979
Maria-CS23 <mariia.pikulina@student.karazin.ua>
2025-05-24 16:07:09 +0300

    pract 8

--- practical work 8/VIEW.sql
+++ practical work 8/VIEW.sql
@@ -25,15 +25,58 @@ WHERE Price > 20000;
 
 SELECT * FROM vPremiumPhones;
 
--- Завдання 6
 
+-- Завдання 6
+UPDATE vPremiumPhones
+SET Price = 30000.00
+WHERE PhoneID = 1;
+
+SELECT * FROM Phone WHERE PhoneID = 1;
 
 
 -- Завдання 7
 
+CREATE VIEW vClientOrders AS
+SELECT 
+    c.ClientID, 
+    CASE 
+        WHEN i.FullName IS NOT NULL THEN i.FullName
+        WHEN le.CompanyName IS NOT NULL THEN le.CompanyName
+        ELSE 'Невідомий клієнт'
+    END AS ClientName,
+    o.OrderID, 
+    o.OrderDate,
+    o.OrderAmount,
+    o.OrderStatus
+FROM Client c
+LEFT JOIN Individual i ON c.ClientID = i.ClientID
+LEFT JOIN LegalEntity le ON c.ClientID = le.ClientID
+JOIN Orders o ON c.ClientID = o.ClientID;
+
+
+SELECT * FROM vClientOrders;
 
 
 -- Завдання 8
+
+CREATE VIEW vClientOrderCounts AS
+SELECT 
+    c.ClientID, 
+    CASE 
+        WHEN i.FullName IS NOT NULL THEN i.FullName
+        WHEN le.CompanyName IS NOT NULL THEN le.CompanyName
+        ELSE 'Невідомий клієнт'
+    END AS ClientName,
+    COUNT(o.OrderID) AS OrderCount
+FROM Client c
+LEFT JOIN Individual i ON c.ClientID = i.ClientID
+LEFT JOIN LegalEntity le ON c.ClientID = le.ClientID
+LEFT JOIN Orders o ON c.ClientID = o.ClientID
+GROUP BY c.ClientID, i.FullName, le.CompanyName;
+
+
+SELECT * FROM vClientOrderCounts;
+
 
 -- Завдання 9
 
